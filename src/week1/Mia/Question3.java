5fe78317ebe9126cbfaa3493a58c6b22287ba487
miasaracuevas <miasaracuevas@yahoo.com>
2024-10-02 17:52:00 -0400

    Q3-Answer

--- src/week1/Mia/Question3.java
+++ src/week1/Mia/Question3.java
@@ -5,7 +5,17 @@ public class Question3 {
 
     public static void main(String[] args) {
 
+            int a = 5;
+            int b = 10;
 
+            System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+            a = a + b; // Step 1: Sum of a and b
+            b = a - b; // Step 2: b becomes original a
+            a = a - b; // Step 3: a becomes original b
+
+            System.out.println("After swapping: a = " + a + ", b = " + b);
+        }
 
 
 
@@ -13,4 +23,3 @@ public class Question3 {
 
 
 
-}
